7002642ed64921f5b8dcd7dacb2dd034bb0e1d53
Svampbob1001 <johans.g@live.se>
2019-05-27 18:56:48 +0200

    Added canvas

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,18 +1,34 @@
-let clickSwitch = false;
-let firstClick = true;
-let orgText;
-let element;
+var clickSwitch = false;
+var firstClick = true;
+var orgText;
+var element;
 
 function myFunction() {
   if (firstClick == true) {
-    element = document.getElementById("demo");
+    element = document.getElementById('dem');
     orgText = element.innerText;
     firstClick = false;
   }
+
   if (clickSwitch == false) {
-    element.innerHTML = "Paragraph changed.";
+    element.innerHTML = 'Paragraph changed.';
   }else {
     element.innerText = orgText;
   }
+
   clickSwitch = !clickSwitch;
 }
+
+function spawnCanvas() {
+  var canvas = document.createElement('canvas');
+  canvas.id = 'cursorLayer';
+  canvas.hight = 200;
+  canvas.width = 200;
+  canvas.style.border = '1px solid';
+  document.body.appendChild(canvas);
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgba(255, 0, 0, 0.2)';
+  ctx.fillRect(20, 20, 80, 80);
+}
+
+// TODO: remove this todo
